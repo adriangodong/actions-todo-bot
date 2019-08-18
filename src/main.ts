cce3c7da5d5a4e200ee3d5f6874a3c917d8e796e
Adrian Godong <adrian.godong@gmail.com>
2019-08-17 21:49:09 -0700

    Ported .NET TODO bot to Node.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,70 @@
 import * as core from '@actions/core';
+import * as github from '@actions/github';
 
-async function run() {
-  try {
-    const myInput = core.getInput('myInput');
-    core.debug(`Hello ${myInput}`);
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+type Args = {
+    repoToken: string;
+}
+
+async function run()
+{
+    try
+    {
+        const args = getAndValidateArgs();
+        const client = new github.GitHub(args.repoToken);
+        const context = github.context;
+        const pull_request = context.payload.pull_request;
+
+        if (!pull_request)
+        {
+            throw new Error("Payload is missing pull_request.");
+        }
+
+        const incompleteTaskListItem = getIncompleteCount(pull_request.body || "");
+
+        await client.repos.createStatus({
+            owner: context.issue.owner,
+            repo: context.issue.repo,
+            sha: pull_request.head.sha,
+            state: incompleteTaskListItem === 0 ? "success" : "error",
+            target_url: "https://github.com/adriangodong/actions-todo-bot",
+            description: incompleteTaskListItem === 0 ? "Ready to merge" : `Found ${incompleteTaskListItem} unfinished task(s)`,
+            context: "Actions TODO"
+        });
+    }
+    catch (error)
+    {
+        core.setFailed(error.message);
+    }
+}
+
+function getAndValidateArgs(): Args
+{
+    return {
+        repoToken: core.getInput('repo-token', { required: true }),
+    }
+}
+
+function getIncompleteCount(pullRequestBody: string)
+{
+    if (!pullRequestBody)
+    {
+        return 0;
+    }
+    const pullRequestBodyLines = pullRequestBody.match(/[^\r\n]+/g);
+    if (pullRequestBodyLines === null)
+    {
+        return 0;
+    }
+
+    let incompleteCount = 0;
+    for (const line of pullRequestBodyLines)
+    {
+        if (line.trim().startsWith("- [ ]"))
+        {
+            incompleteCount++;
+        }
+    }
+    return incompleteCount;
 }
 
 run();
